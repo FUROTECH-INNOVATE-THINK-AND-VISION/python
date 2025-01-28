84da3e73a5e0afc542f1f59a372e6fdb10adaeb0
FUROTECH <yousuf.alimran09@gmail.com>
2025-01-28 16:34:02 +0500

    Add files via upload

---
+++ First program.py
@@ -0,0 +1,15 @@
+print("Yousuf")
+print("My age is 15.7")
+print(15)
+print(15+17)
+name = "Yousuf"
+age = 15
+price = 25.99
+print(name)
+print(age)
+print(price)
+print("My name is : ", name)
+print("My age is : ", age)
+print("The price  is : ", price)
+
+
